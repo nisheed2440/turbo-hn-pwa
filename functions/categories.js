9ae1db8e667e6740d43e47fac04ecdcbb6d734ae
nisheed.jagadish <nisheed.jagadish@loblaw.ca>
2020-09-11 12:34:21 -0400

    Added category changes

--- functions/categories.js
+++ functions/categories.js
@@ -8,15 +8,16 @@ const categoryTemplate = ejs.compile(`
     <div class="row">
         <% items.forEach(function(item){ %>
             <div class="col-md-4">
-                <div class="card mb-4 shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="<%- item.label %>"><title><%- item.label %></title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em"><%- item.label %></text></svg>
-                    <div class="card-body">
-                    <p class="card-text"><%- item.description %></p>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <div class="btn-group">
-                            <a href="/plp/<%- item.id %>" class="btn btn-sm btn-outline-secondary">View</a>
-                        </div>
-                    </div>
+				<div class="card mb-4 shadow-sm">
+					<img src="<%- item.imageUrl %>" class="card-img-top" alt="<%- item.label %>">
+					<div class="card-body">
+					<h5 class="card-title"><%- item.label %></h5>
+						<p class="card-text"><%- item.description %></p>
+						<div class="d-flex justify-content-between align-items-center">
+							<div class="btn-group">
+								<a href="/plp/<%- item.id %>" class="btn btn-sm btn-outline-secondary">View</a>
+							</div>
+						</div>
                     </div>
                 </div>
             </div>
